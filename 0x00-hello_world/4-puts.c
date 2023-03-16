4ffd414471ef1a1925e923134e6ba6a2148c72f0
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-16 23:00:03 +0100

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* prints exactly "Programming is like building a multilingual puzzle, followed by a new line */
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
