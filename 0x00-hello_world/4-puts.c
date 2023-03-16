9d1ac651fc4be642b94dbf88971d03da57954a91
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-16 23:15:27 +0100

    Conform 4-puts.c to Bitty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	/* prints exactly "Programming is like building a multilingual puzzle, followed by a new line */
-
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
