b3784335b21d3fe2b191a3168fbc01ecf993053a
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-18 15:36:09 +0100

    Add 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c1 = '0';
+	int c2;
+	int c3;
+
+	while (c1 <= '9')
+	{
+
+		c2 = c1 + 1;
+		c3 = c2 + 1;
+		while (c2 <= '9')
+		{
+			while (c3 <= '9')
+			{
+				putchar(c1);
+				putchar(c2);
+				putchar(c3);
+				if (c1 != '7' || c2 != '8' || c3 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c3++;
+			}
+			c2++;
+		}
+		c1++;
+	}
+	putchar('\n');
+	return (0);
+}
