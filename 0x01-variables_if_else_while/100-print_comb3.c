3b49a53a8ef0e863d60d88de61c349fbdbf7312e
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-18 14:45:10 +0100

    Add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c1 = '0';
+	int c2;
+
+	while (c1 < '8')
+	{
+		c2 = c1 + 1;
+		while (c2 <= '9')
+		{
+			putchar(c1);
+			putchar(c2);
+			putchar(',');
+			putchar(' ');
+			c2++;
+		}
+		c1++;
+	}
+	putchar(c1);
+	putchar(c1 + 1);
+	putchar('\n');
+	return (0);
+}
