c7f71771b7ff29c08214129468eb387258578194
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-18 14:06:45 +0100

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '9')
+			printf("%c", c);
+		else
+			printf("%c, ", c);
+	}
+	putchar('\n');
+	return (0);
+}
