1cd65d363bdd19925ecea64cd5dda5e4b354a29d
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-22 00:15:08 +0000

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int n = 0;
+	while (n < 2359)
+	{
+		while ((n % 100) <= 59)
+		{
+			_putchar('0' + (n / 1000));
+			_putchar('0' + ((n / 100) % 10));
+			_putchar(':');
+			_putchar('0' + ((n % 100) / 10));
+			_putchar('0' + (n % 10));
+			_putchar('\n');
+			n++;
+		}
+		n += 40;
+	}
+}
