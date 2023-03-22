d971ea8230c16b4ed622e446b04c219b519952ad
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-22 23:05:36 +0000

    Add 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: number to be added
+ * @j: number to be added
+ *
+ * Return: result of @i + @j
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
