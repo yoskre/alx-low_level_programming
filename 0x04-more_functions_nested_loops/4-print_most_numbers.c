63c3074bf2b44eb0b96a2ba79657b4861e9f4f14
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-24 13:39:29 +0000

    Add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9 and a new line
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		if ((i != '2') && (i != '4'))
+			_putchar(i);
+	_putchar('\n');
+}
