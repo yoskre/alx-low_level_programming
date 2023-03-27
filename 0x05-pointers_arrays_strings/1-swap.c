f9e07bb91150df58519c8fa031854353244c85f6
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-27 23:42:50 +0000

    Add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap value of two variables
+ * @a: variable 1
+ * @b: variable 2
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *tmp = a;
+
+	a = b;
+	b = tmp;
+}
