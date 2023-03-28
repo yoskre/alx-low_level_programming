87316f7cde1d85c5d5f731890226769a87b6c8ef
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-28 17:47:53 +0000

    Add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _atoi - convert string @s to integer
+ * @s: string to be converted
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int n = 0;
+	int coef = 10;
+	int found = 0;
+
+	while ((*s != 0))
+	{
+		if (found != 0 && !((*s >= '0') && (*s <= '9')))
+		{
+			break;
+		}
+		/* add a sign if it exists */
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		if ((*s >= '0') && (*s <= '9'))
+		{
+			if (found == 1)
+				n *= coef;
+			n += *s - '0';
+			found = 1;
+		}
+		if (!((*s >= '0') && (*s <= '9')) && (*s != '-'))
+		{
+			sign = 1;
+		}
+		s++;
+	}
+	n *= sign;
+	return (n);
+}
