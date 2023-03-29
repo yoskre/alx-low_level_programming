68abb612c22d99a56e91c9c4afc4457637e16b25
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-29 23:00:36 +0000

    Add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode string @str into 1337
+ * @str: string
+ *
+ * Return: pointer to @str
+ */
+char *leet(char *str)
+{
+	char *p = str;
+	char leet[10][10] = {
+			{'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'},
+			{'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'},};
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i < 10; i++)
+			if (*str == leet[0][i])
+				*str = leet[1][i];
+		str++;
+	}
+	return (p);
+}
