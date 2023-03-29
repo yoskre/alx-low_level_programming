9d09ddcbc79e100a8c8b3802943e1d7b2fefa307
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-29 21:05:05 +0000

    Add 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of array @a
+ * @a: array to be reversed
+ * @n: number of the elements in @a
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int p1 = 0;
+	int p2 = n - 1;
+	int tmp;
+
+	while (p1 != p2)
+	{
+		tmp = a[p1];
+		a[p1] = a[p2];
+		a[p2] = tmp;
+		p1++;
+		p2--;
+	}
+}
