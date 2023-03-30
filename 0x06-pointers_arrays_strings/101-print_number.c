9c64c24b75fcb98534a7dc47baa31bcc5688d27b
yoskre <125600404+yoskre@users.noreply.github.com>
2023-03-30 23:53:37 +0000

    Add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * print_number - prints @n
+ * @n: integer
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int coef = 10;
+	
+	while (n / coef)
+		coef *= 10;
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	while (coef / 10)
+	{
+		coef /= 10;
+		_putchar('0' + (n / coef) % 10);
+	}
+}
