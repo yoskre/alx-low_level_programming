3e90ff2a5de5d3c1de52012016bb934ede2919ac
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-03 22:27:04 +0000

    Add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - find first occurence of @c in @s
+ * @s: string
+ * @c: character
+ *
+ * Return: first @c occurence
+ */
+char *_strchr(char *s, char c)
+{
+	while (s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
