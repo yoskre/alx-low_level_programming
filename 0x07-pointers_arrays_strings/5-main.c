cbac70ea9fa1e5ebe53c805a8463da5e0b14deec
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-04 22:36:39 +0000

    Fix 5-strstr.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,11 +8,7 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strstr(s, f);
+    char *t = _strstr("First, solve the problem. Then, write the code.", "solve");
     printf("%s\n", t);
     return (0);
 }
