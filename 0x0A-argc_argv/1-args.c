d1642873ae1bb29ff62b0e686ce40a0def2bc096
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-10 23:28:27 +0000

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument values
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%d\n", argc - 1);
+	return (0);
+}
