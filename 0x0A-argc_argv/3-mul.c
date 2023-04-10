72987d7548703aa3e372cf742c9780f697c685df
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-10 23:50:02 +0000

    Add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument values
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	if ((argc - 1) != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
