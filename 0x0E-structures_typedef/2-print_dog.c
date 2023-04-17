f78971080332ef41763d0e588ed5d12d0771ff28
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-17 23:45:21 +0000

    2nd fix 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\nAge: %.6f\nOwner: %s",
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n",
 			d->name ? d->name : "(nil)",
 			d->age,
 			d->owner ? d->owner : "(nil)");
