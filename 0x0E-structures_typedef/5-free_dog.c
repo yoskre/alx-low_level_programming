308a35eade5177477fd7874c04dac265e6b1f887
yoskre <125600404+yoskre@users.noreply.github.com>
2023-04-18 00:18:15 +0000

    Add 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - free the doggo!
+ * @d: dog struct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
