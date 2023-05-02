921001d0ab855e12b9be77fd3499ef621b664b6c
yoskre <125600404+yoskre@users.noreply.github.com>
2023-05-02 23:32:54 +0100

    3rd fix 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ void delete_nodeint(listint_t *pa, listint_t *pb)
 {
 	if (pa != NULL)
 	{
-		if (pb != NULL)
+		if (pb->next != NULL)
 			pa->next = pb->next;
 		else
 			pa->next = NULL;
@@ -41,7 +41,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		for (i = 0; i < index && pointer != NULL; i++)
 		{
-			if (i + 1 == index)
+			if (i + 1 == index && pointer->next != NULL)
 			{
 				delete_nodeint(pointer, pointer->next);
 				return (1);
